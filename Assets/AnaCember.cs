942f431619f5c9ce286743b7910f23767052569f
sedanur55 <seda.drms@hotmail.com>
2019-10-13 22:23:20 +0300

    unityProject3
    aa-unity projesi

---
+++ Assets/AnaCember.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnaCember : MonoBehaviour
+{
+    public GameObject kucukCember;
+    GameObject oyunYoneticisi;
+    AudioSource ses;
+    void Start()
+    {
+        oyunYoneticisi = GameObject.FindGameObjectWithTag("oyunyoneticisitag");
+        ses = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            kucukCemberOlustur();
+        }
+    }
+    void kucukCemberOlustur()
+    {
+        ses.Play();
+        Instantiate(kucukCember, transform.position, transform.rotation);
+        
+        oyunYoneticisi.GetComponent<OyunYoneticisi>().kalanText();
+    }
+}
---
+++ Assets/OyunYoneticisi.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OyunYoneticisi : MonoBehaviour
+{
+    GameObject donenCember;
+    public AnaCember anaCember;
+    public Animator animator;
+    public Text donenCemverText;
+    public Text bir;
+    public Text iki;
+    public Text uc;
+    public int kacAtıs;
+    bool kontrol = true;
+    AudioSource carpisma;
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("kayit", int.Parse(SceneManager.GetActiveScene().name));
+        donenCember = GameObject.FindGameObjectWithTag("donencembertag");
+        donenCemverText.text = SceneManager.GetActiveScene().name;
+        carpisma = GetComponent<AudioSource>();
+
+        if (kacAtıs < 2)
+        {
+            bir.text = kacAtıs + "";
+        }
+        else if (kacAtıs < 3)
+        {
+            bir.text = kacAtıs + "";
+            iki.text = (kacAtıs - 1) + "";
+        }
+        else
+        {
+            bir.text = kacAtıs + "";
+            iki.text = (kacAtıs - 1) + "";
+            uc.text = (kacAtıs - 2) + "";
+        }
+    }
+    public void kalanText()
+    {
+        kacAtıs--;
+
+        if (kacAtıs < 2)
+        {
+            bir.text = kacAtıs + "";
+            iki.text = "";
+            uc.text = "";
+        }
+        else if (kacAtıs < 3)
+        {
+            bir.text = kacAtıs + "";
+            iki.text = (kacAtıs - 1) + "";
+            uc.text = "";
+        }
+        else
+        {
+            bir.text = kacAtıs + "";
+            iki.text = (kacAtıs - 1) + "";
+            uc.text = (kacAtıs - 2) + "";
+        }
+        if (kacAtıs == 0)
+        {
+            StartCoroutine(yeniLevel());
+        }
+    }
+    IEnumerator yeniLevel()
+    {
+        donenCember.GetComponent<Dondurme>().enabled = false;
+        anaCember.GetComponent<AnaCember>().enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        if (kontrol)
+        {
+            animator.SetTrigger("yenilevel");
+            yield return new WaitForSeconds(1.5f);
+            SceneManager.LoadScene(int.Parse(SceneManager.GetActiveScene().name) + 1);
+        }
+       
+    }
+
+
+    public void OyunBitti()
+    {
+        Debug.Log("0");
+        donenCember.GetComponent<Dondurme>().enabled = false;
+        Debug.Log("1");
+
+        anaCember.enabled = false;
+        Debug.Log("2");
+
+        carpisma.Play();
+        Debug.Log("3");
+
+        animator.SetTrigger("oyunbitti");
+        Debug.Log("4");
+
+        kontrol = false;
+        Invoke("CagirilanMetot",2);
+    }
+
+    void CagirilanMetot()
+    {
+        SceneManager.LoadScene("AnaMenu");
+    }
+
+    
+}
